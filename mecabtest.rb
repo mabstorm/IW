1d24307ce7f371f1e6b579d71119a4941ac0db09
mabstorm <mabstorm@yahoo.com>
2011-11-21 01:48:08 -0500

    added pattern finder

--- mecabtest.rb
+++ mecabtest.rb
@@ -1,26 +1,73 @@
 #!/usr/bin/ruby
 
 require 'MeCab'
+
+
+class SenPos
+  attr_accessor :sentence, :pos, :counts
+  def initialize n
+    @sentence = Array.new(size=n)
+    @pos = Array.new(size=n)
+    @counts = Hash.new(0)
+  end
+end
+
+
+
+# main
+if __FILE__ == $0
+
 sentence = "太郎はこの本を二郎を見た女性に渡した。"
 
 c = MeCab::Tagger.new()
 
+tags = Hash.new # obtain all types of tags found
+
 ARGV.each do |arg|
   fp = File.open(arg,'r')
+  new_filename = "#{arg[0...35]}-pos.txt"
+  next if File.exist? new_filename
+  fpo = File.open(new_filename,'w+')
   data = fp.readlines
+  final_ar = Array.new(size=data.length)
+  final_i = 0
+
   data.each do |sentence|
-    puts sentence
-    puts c.parse(sentence)
+    #puts sentence
+    #puts c.parse(sentence)
 
     n = c.parseToNode(sentence)
+    
 
+    index = 0;
+    sp = SenPos.new(n.length)
     while n do
+      pos_tag = n.feature.match(/^[^,]*/)[0]
+      if pos_tag=="BOS/EOS"
+        n = n.next
+        next
+      end
       #print n.surface, "\t", n.feature, "\t", n.cost, "\n"
-      print n.surface, "\t", n.feature.split(",").first
+      sp.sentence[index] = n.surface
+      sp.pos[index] = pos_tag
+      sp.counts[pos_tag]+=1
+      if tags[pos_tag].nil?
+        tags[pos_tag] = pos_tag
+      end
       n = n.next
+      index+=1
     end
-    puts ''
-    break
+    #arprint sp.pos
+    if sp.counts["形容詞"] + sp.counts["連体詞"] > 2
+      #arprint sp.sentence
+      final_ar[final_i] = "#{sp.sentence.join(" ")}-divider-#{sp.pos.join(" ")}"
+      final_i+=1
+    end
   end
+  fpo.puts final_ar
 end
+  puts "---------------------\n------------------"
+  puts tags.keys
+end
+
 
---
+++ patterns.rb
@@ -0,0 +1,100 @@
+#!/usr/bin/ruby
+
+require 'pp'
+require 'yaml'
+
+
+
+def make_patterns
+
+all_patterns = Hash.new
+pre = 0
+mid = 1
+post = 2
+
+if ARGV.empty?
+  to_run = "./tsubame00.kototoi.org/doc0000000000-pos.txt"
+else
+  to_run = ARGV
+end
+
+
+to_run.each do |arg|
+  fp = File.open(arg,'r')
+  dirnum = arg[7...8]
+  filenum = arg[33...35]
+  sentences = fp.readlines
+  sentences.each do |line|
+    temp = line.split("-divider-")
+    sentence = temp[0]
+    pos_linked = temp[1]
+    next if pos_linked.nil?
+    words = sentence.split
+    found_adjs = Array.new
+    num_adjs = 0
+    pos = pos_linked.split
+    pos.each_index do |i|
+      if pos[i]=="形容詞" || pos[i]=="連体詞"
+        found_adjs[num_adjs] = i
+        num_adjs+=1
+      end
+    end
+    (0..words.length-1).each do |window_size|
+      words.each_index do |i|
+        num_above = found_adjs.select {|v| v>(i+window_size)}.length
+        num_below = found_adjs.select {|v| v<i}.length
+        this_word = words[i..i+window_size].join(" ") if i+window_size < words.length
+        this_pos = pos[i..i+window_size].join(" ") if i+window_size < words.length
+        key = "#{this_word}--#{this_pos}"
+        if num_above > 1
+          all_patterns[key] = [0,0,0] if all_patterns[key].nil?
+          all_patterns[key][post]+=1
+        end
+        if num_below > 1
+          all_patterns[key] = [0,0,0] if all_patterns[key].nil?
+          all_patterns[key][pre]+=1
+        end
+        if num_above > 0 && num_below > 0
+          all_patterns[key] = [0,0,0] if all_patterns[key].nil?
+          all_patterns[key][mid]+=1
+        end
+      end
+    end
+  end
+end
+
+puts "\n\n\n++++++++++++PRES++++++++++++++++"
+puts "\n\n\n"
+pres = all_patterns.sort_by {|k,v| v[0]}.reverse
+
+
+pres.each do |ar|
+  puts "#{ar[0]}\t#{ar[1][0]}"
+end
+
+puts "\n\n\n++++++++++++MIDS++++++++++++++++"
+puts "\n\n\n"
+mids = all_patterns.sort_by {|k,v| v[1]}.reverse
+mids.each do |ar|
+  puts "#{ar[0]}\t#{ar[1][1]}"
+end
+
+puts "\n\n\n++++++++++++POSTS++++++++++++++++"
+
+puts "\n\n\n"
+posts = all_patterns.sort_by {|k,v| v[2]}.reverse
+posts.each do |ar|
+  puts "#{ar[0]}\t#{ar[1][2]}"
+end
+
+
+
+
+end
+
+
+# main
+if __FILE__ == $0
+  make_patterns
+end
+
