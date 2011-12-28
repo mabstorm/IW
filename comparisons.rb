7ead94b51004bafbbd267dd55a848f0c0eddaf94
Michael Bailey <mabstorm@yahoo.com>
2011-12-27 23:15:47 -0600

    made code and pseudocode for actually finding comparisons within the corpus

--- comparisons.rb
+++ comparisons.rb
@@ -5,6 +5,30 @@ require './get_adj_groups.rb'
 require 'pp'
 
 
+pattern_file = './jpn_structures'
+patterns = File.open(pattern_file,'r').readlines
+
 adjs = get_groups_simple
-pp adjs
+
+counts = Hash.new
+
+ARGV.each do |arg|
+  sentences = File.open(arg,'r').readlines
+  sentences.each do |sentence|
+    adjs.each do |adj_a|
+      adjs.each do |adj_b|
+        next if adj_a==adj_b
+        next if /sentence does not have both/
+        patterns.each do |pattern|
+          fst_pattern = pattern.sub("{A}",adj_a).sub("{B}", adj_b)
+          sec_pattern = pattern.sub("{A}",adj_b).sub("{B}", adj_a)
+          counts[fst_pattern]+=1 if sentence.match(fst_pattern)
+          counts[sec_pattern]+=1 if sentence.match(sec_pattern)
+        end
+      end
+    end
+  end
+end
+
+
 
