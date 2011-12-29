51a8ba3afce0066c704ca5b61faee78025d0879d
mabstorm <mabstorm@yahoo.com>
2011-12-29 14:41:10 -0500

    fixed bug in quick find, uploading results

--- quick_find.rb
+++ quick_find.rb
@@ -7,25 +7,32 @@ require 'pp'
 
 def inc_counts(item)
   @counts[item] = 0 if @counts[item].nil?
-  @counts+=1
+  @counts[item]+=1
 end
 
 
 word_a = "大きい"
 word_b = "巨大"
-pattern_a = "#{word_a}.*#{word_b}"
-pattern_b = "#{word_b}.*#{word_a}"
+pattern_a = "#{word_a}[^、]*#{word_b}"
+pattern_b = "#{word_b}[^、]*#{word_a}"
+filenm = 0
+ttlfl = ARGV.length
 ARGV.each do |arg|
+  filenm+=1
   lines = File.open(arg, 'r').readlines
+  linenum = 0
+  totallines = lines.length
   lines.each do |line|
+    linenum+=1
+    $stderr.print "\r#{linenum} / #{totallines} : #{filenm} / #{ttlfl}" if linenum % 500 == 0
     line = line.split('-divider-')
     next if line[1].nil?
     sentence = line[0].strip
     next if (sentence.match(word_a).nil? || sentence.match(word_b).nil?)
     m = sentence.match(pattern_a)
-    inc_counts(m) if !m.nil?
+    inc_counts(m.string) if !m.nil?
     m2 = sentence.match(pattern_b)
-    inc_counts(m2) if !m2.nil?
+    inc_counts(m2.string) if !m2.nil?
   end
 end
 
